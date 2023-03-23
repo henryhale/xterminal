3e100337138b7270c5aaf860b972c7c8d28a9ed5
henryhale <henryharrison8117@gmail.com>
2023-03-23 16:01:02 +0300

    2 files modified

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,46 @@
-const { copyFileSync, readFileSync } = require("node:fs");
-const terser = require("@rollup/plugin-terser");
-const babel = require("@rollup/plugin-babel");
-const replace = require("@rollup/plugin-replace");
+const { copyFileSync, readFileSync } = require('node:fs');
+const terser = require('@rollup/plugin-terser');
+const babel = require('@rollup/plugin-babel');
+const replace = require('@rollup/plugin-replace');
 
 const pkg = (() => {
-    return JSON.parse(readFileSync('./package.json', 'utf8'));
+  return JSON.parse(readFileSync('./package.json', 'utf8'));
 })();
 
+const banner = `
+/**
+ *  XTerminal - v${pkg.version}
+ *  @author Henry Hale
+ *  @license MIT
+ *  -> https://github.com/henryhale/xterminal
+ */`;
+
 function copyToDist() {
-    return {
-      closeBundle: () => {
-        copyFileSync('./LICENSE', './dist/LICENSE');
-        console.log(`[Y]: copied license file to dist/`);
-        copyFileSync('./types/terminal.d.ts', './dist/types.d.ts');
-        console.log(`[Y]: copied typings file to dist/`);
-      }
-    }
+  return {
+    closeBundle: () => {
+      copyFileSync('./LICENSE', './dist/LICENSE');
+      console.log(`[Y]: copied license file to dist/`);
+      copyFileSync('./types/terminal.d.ts', './dist/types.d.ts');
+      console.log(`[Y]: copied typings file to dist/`);
+    },
+  };
 }
 
 module.exports = {
-    input: 'out/index.js',
-    output: {
-        name: 'XTerminal',
-        file: pkg.browser,
-        format: 'umd'
-    },
-    plugins: [
-        replace({
-            preventAssignment: true,
-            values: { __VERSION__: pkg.version }
-        }),
-        babel({ babelHelpers: 'bundled' }), 
-        terser(),
-        copyToDist()
-    ]
+  input: 'out/index.js',
+  output: {
+    name: 'XTerminal',
+    file: pkg.browser,
+    format: 'umd',
+    banner,
+  },
+  plugins: [
+    replace({
+      preventAssignment: true,
+      values: { __VERSION__: pkg.version },
+    }),
+    babel({ babelHelpers: 'bundled' }),
+    terser(),
+    copyToDist(),
+  ],
 };
