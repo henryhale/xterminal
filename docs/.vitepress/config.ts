12a479bee2c2b62c0ff79a26f65b2e57d921c080
Henry Hale <henryharrison8117@gmail.com>
2023-05-09 10:43:26 -0500

    docs: updated config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -50,10 +50,7 @@ export default defineConfig({
       },
       {
         text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
+        items: []
       }
     ],
 
