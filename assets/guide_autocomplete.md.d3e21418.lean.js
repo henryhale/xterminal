9f55ea3ec62419856a9a2f876b624ec1cf687d7e
henryhale <henryhale@users.noreply.github.com>
2023-07-06 16:02:28 +0000

    deploy: 026a72645c12ed2173e3c261b3df573e30220907

---
+++ assets/guide_autocomplete.md.d3e21418.lean.js
@@ -0,0 +1,10 @@
+import{_ as n,H as t,o,c as l,L as p,w as c,V as e,k as s}from"./chunks/framework.1b5edb90.js";const g=JSON.parse('{"title":"AutoComplete","description":"","frontmatter":{},"headers":[],"relativePath":"guide/autocomplete.md","filePath":"guide/autocomplete.md"}'),r={name:"guide/autocomplete.md"},i=e("",16),y=s("pre",null,[s("code",null,`[user] $ help
+help
+[user] $ hack
+hack
+[user] $ ls
+ls
+[user] $ history
+history
+[user] $ h▊
+`)],-1),h=e("",2);function d(F,D,A,u,C,m){const a=t("browser-preview");return o(),l("div",null,[i,p(a,null,{default:c(()=>[y]),_:1}),h])}const f=n(r,[["render",d]]);export{g as __pageData,f as default};
