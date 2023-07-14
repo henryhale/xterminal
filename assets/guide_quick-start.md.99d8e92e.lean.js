1a1d282ae0933f04bd3b641a40a493cd7e75b556
henryhale <henryhale@users.noreply.github.com>
2023-07-14 21:05:54 +0000

    deploy: 737ba589ce1e0038fa9e778e30f0a79e5d176f96

---
+++ assets/guide_quick-start.md.99d8e92e.lean.js
@@ -0,0 +1,3 @@
+import{_ as l,H as p,o as e,c as o,L as t,w as r,V as n,k as s}from"./chunks/framework.1b5edb90.js";const g=JSON.parse('{"title":"Quick Start","description":"","frontmatter":{},"headers":[],"relativePath":"guide/quick-start.md","filePath":"guide/quick-start.md"}'),c={name:"guide/quick-start.md"},F=n("",6),D=s("pre",null,[s("code",null,`Hello World!
+[user] $ ▊
+`)],-1),i=n("",4);function y(u,C,b,m,A,d){const a=p("browser-preview");return e(),o("div",null,[F,t(a,null,{default:r(()=>[D]),_:1}),i])}const E=l(c,[["render",y]]);export{g as __pageData,E as default};
