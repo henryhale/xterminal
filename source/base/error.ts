e6c5ecf995a2309089ff5b6ed96daba1fdbbcfbd
Henry Hale <henryharrison8117@gmail.com>
2023-10-02 18:28:07 -0500

    chore: remove event errors

--- source/base/error.ts
+++ source/base/error.ts
@@ -6,12 +6,6 @@ export class XError extends Error {
     }
 }
 
-export const EVENT_NAME_ERR =
-    "EventEmitter: The event name (first argument) should either be a string or symbol";
-
-export const EVENT_FUNC_ERR =
-    "EventEmitter: The event listener (second argument) is required and must be a function";
-
 export const TARGET_INVALID_ERR =
     "mount: A parent HTMLElement (target) is required";
 
