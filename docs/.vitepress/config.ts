69bd494f08e4bf8bd56c1243874bf533d79cd1a8
Henry Hale <henryharrison8117@gmail.com>
2023-10-07 16:36:58 +0000

    fix(docs): update base url prefix

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/xterminal/',
+  base: '/',
 
   // metadata
   lang: 'en-US',
