efa571b9b37fc56b5106ac8d6c269dede8b2a26a
henryhale <henryhale@users.noreply.github.com>
2026-01-19 19:45:10 +0000

    deploy: 635f3ba4a4c222e9cdf33b3e9a269e7c925159e6

---
+++ assets/guide_autocomplete.md.D8dzWx6a.lean.js
@@ -0,0 +1,10 @@
+import{_ as i,C as n,c as h,o as l,ag as e,G as o,w as p,j as t}from"./chunks/framework.bCALENAD.js";const m=JSON.parse('{"title":"AutoComplete","description":"","frontmatter":{},"headers":[],"relativePath":"guide/autocomplete.md","filePath":"guide/autocomplete.md"}'),r={name:"guide/autocomplete.md"};function d(k,s,c,y,E,u){const a=n("browser-preview");return l(),h("div",null,[s[1]||(s[1]=e("",13)),o(a,null,{default:p(()=>[...s[0]||(s[0]=[t("pre",null,[t("code",null,`[user] $ help
+help
+[user] $ hack
+hack
+[user] $ ls
+ls
+[user] $ history
+history
+[user] $ h▊
+`)],-1)])]),_:1}),s[2]||(s[2]=e("",1))])}const b=i(r,[["render",d]]);export{m as __pageData,b as default};
---
+++ assets/guide_quick-start.md.wcgn-Xhz.lean.js
@@ -0,0 +1,3 @@
+import{_ as l,C as p,c as t,o as e,ag as i,G as h,w as k,j as a}from"./chunks/framework.bCALENAD.js";const b=JSON.parse('{"title":"Quick Start","description":"","frontmatter":{},"headers":[],"relativePath":"guide/quick-start.md","filePath":"guide/quick-start.md"}'),r={name:"guide/quick-start.md"};function E(d,s,g,c,y,o){const n=p("browser-preview");return e(),t("div",null,[s[1]||(s[1]=i("",5)),h(n,null,{default:k(()=>[...s[0]||(s[0]=[a("pre",null,[a("code",null,`Hello World!
+[user] $ ▊
+`)],-1)])]),_:1}),s[2]||(s[2]=i("",4))])}const m=l(r,[["render",E]]);export{b as __pageData,m as default};
